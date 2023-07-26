4865b8a7fdfd5a44623e3b5d7819016a12bd02c2
renad1 <renadxx17@gmail.com>
2023-07-26 14:38:42 +0300

    lab

---
+++ q1.py
@@ -0,0 +1,7 @@
+numbers = range(45,210)
+for number in numbers:
+    if number == 100:
+        continue 
+    elif number == 205:
+        break
+    print(number)
---
+++ q2.py
@@ -0,0 +1,5 @@
+puzzle = 'what is the product of 7 * 24 ? '
+while input(puzzle) != '168':
+    print("Your Answer is wrong try again..")
+else:
+    print("You answered this Question correctly")
