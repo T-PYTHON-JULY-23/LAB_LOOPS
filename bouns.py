c134d3ddf160d1f616007b7156e039514299193b
Lenah11 <lenhh11@gmail.com>
2023-07-26 23:18:47 +0300

    Bouns_SumEvenNumber

---
+++ bouns.py
@@ -0,0 +1,24 @@
+
+
+n = int(input("Enter a positive integer :"))
+
+S = 0
+
+for n in  range(1,n+1):
+    if n %2 == 0:
+        S = S + n
+print(f"The sum of even numbers between 1 and {n} is {S}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
