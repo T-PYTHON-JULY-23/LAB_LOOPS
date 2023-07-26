6c7593a0d1b1c9dc8bc8900f90db10f32e5474f6
Banan Alburaidi <bananburaidi@gmail.com>
2023-07-26 18:05:02 +0300

    adding new file

---
+++ bonus.py
@@ -0,0 +1,11 @@
+
+
+
+enteringNumber=int(input("Enter a number :"))
+sum=0
+
+for i in range(2, enteringNumber+1, 2):
+        sum=sum+i
+     
+
+print(sum)
---
+++ week1.py
@@ -0,0 +1,14 @@
+
+for number in range(45,210) :
+    
+    if number==205: break
+    if number==100 :continue 
+    print(number)
+
+
+question="what is the product of 7 * 24 ?"
+while int(input(question)!= "168"):
+    print("Your Answer is wrong try again..")
+else :
+  print("You answered this Question correctly")   
+
