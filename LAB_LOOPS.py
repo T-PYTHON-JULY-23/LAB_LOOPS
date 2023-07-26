b02286fe8cddb23b1329e0adbf92ef6c63a7c99e
nada <nada_abdullah_8@hotmail.com>
2023-07-26 20:48:38 +0300

    savelab

---
+++ LAB_LOOPS.py
@@ -0,0 +1,28 @@
+
+
+
+for elements in range(45,210):
+    if elements!=100:
+       print (elements)
+    
+    if elements ==205:
+     break
+
+
+
+print("_"*50)
+
+userQ:int="what is the product of 7 * 24 ?"
+
+while int(input(userQ)) != 168 :
+   print("Your Answer is wrong try again..")
+
+else:
+    print("You answered this Question correctly")
+
+   
+   
+
+
+
+
