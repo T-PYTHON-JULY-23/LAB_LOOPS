d67a6b27eb88816d049021b1cd0de2e4bc5e59f8
Lenah11 <lenhh11@gmail.com>
2023-07-26 14:39:47 +0300

    Labs_Range_Lenah

---
+++ labs_while_loop.py
@@ -0,0 +1,6 @@
+Question= "what is the product of 7 * 24 ?"
+
+while int(input(Question)) != 168:
+    print("Your Answer is wrong try again..")
+else:
+    print("You answered this Question correctly")
