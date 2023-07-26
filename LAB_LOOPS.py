1d3f8ada3874e77a3bd084d653b8f78dd91936c6
naif <naif.albariqi201@hotmail.com>
2023-07-26 16:34:23 +0300

    lab && Bonus --N--

---
+++ LAB_LOOPS.py
@@ -0,0 +1,8 @@
+range = range(45, 210)
+for i in range:
+    if i == 100:
+        continue
+    elif i == 205:
+        break
+    print(i)
+
---
+++ LAB_LOOPS2.py
@@ -0,0 +1,6 @@
+puzzle = "what is the product of 7 * 24 ? "
+n = 7*24
+while int(input(puzzle)) != n:
+    print("PYour Answer is wrong try again..")
+else:
+    print("You answered this Question correctly")
