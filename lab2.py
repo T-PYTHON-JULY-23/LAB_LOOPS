39c5540fc2a39e84b030315c0b6d659509fc61d5
Basma <129796403+basiix1@users.noreply.github.com>
2023-07-27 10:24:48 +0300

    update

--- lab2.py
+++ lab2.py
@@ -1,6 +1,6 @@
 while True:
-    answer = input("What is the product of 7 * 24? ")
-    if answer == "168":
+    answer =int(input("What is the product of 7 * 24? "))
+    if answer == 168:
         print("You answered this question correctly!")
         break
     else:
